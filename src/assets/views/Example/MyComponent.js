77a0e5fb3734a79c55fb48832e5c120e6ed5eada
Nguyen Huu Thang <nht221@gmail.com>
2022-04-25 15:27:43 +0700

    update

---
+++ src/assets/views/Example/MyComponent.js
@@ -0,0 +1,72 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import ChildComponent from './ChildComponent'
+
+class MyComponent extends React.Component {
+    //key:value
+    state = {
+        firstName: '',
+        lastName: '',
+        arrJobs: [
+            { id: 'abcJob1', title: 'Developers', salary: '500 $' },
+            { id: 'abcJob2', title: 'Testers', salary: '400 $' },
+            { id: 'abcJob3', title: 'Project managers', salary: '1000 $' }
+        ]
+    }
+
+    /* 
+    JSX => return block
+    fragment
+    */
+    handleChangeFirstName = (event) => {
+        this.setState({
+            firstName: event.target.value
+        })
+    }
+    handleChangeLastName = (event) => {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
+    handleSubmit = (event) => {
+        event.preventDefault()
+        console.log('>>> check data input: ', this.state)
+    }
+    //re-render
+    render() {
+        console.log('>>> call render: ', this.state)
+        return (
+            <>
+                <form>
+                    <label htmlFor="fname">First name:</label><br />
+                    <input
+                        type="text"
+                        value={this.state.firstName}
+                        onChange={(event) => this.handleChangeFirstName(event)}
+                    />
+                    <br />
+                    <label htmlFor="lname">Last name:</label><br />
+                    <input
+                        type="text"
+                        value={this.state.lastName}
+                        onChange={(event) => this.handleChangeLastName(event)}
+                    /><br /><br />
+                    <input type="submit"
+                        onClick={(event) => this.handleSubmit(event)}
+                    />
+                </form>
+
+                <ChildComponent
+                    
+                    name={this.state.firstName}
+                    age={'25'}
+
+                    address={'Ha noi'}
+                    arrJobs={this.state.arrJobs}
+                />
+                       </>
+        )
+    }
+}
+
+export default MyComponent;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './assets/views/App';
 import reportWebVitals from './reportWebVitals';
+import './assets/styles/global.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
