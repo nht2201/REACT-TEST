2f304759687a9ccf3b1aff1df917c5df27960af6
Nguyen Huu Thang <nht221@gmail.com>
2022-05-05 09:15:38 +0700

    update

--- src/assets/views/Example/Home.js
+++ src/assets/views/Example/Home.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Color from "../HOC/Color";
-
+import anh from "../../images/123.png";
 class Home extends React.Component {
 
     render() {
         return (
+            <>
             <div>
                WELCOME TO VNPT TECHNOLOGY
             </div>
+            <div>
+                <img src={anh} style={{ width:'300px', height:'300px', marginTop:'-80px' }}/>
+            </div>
+            </>
         )
     }
 }
